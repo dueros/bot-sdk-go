9c6b5233545103d85bd1bee7b7ce759e7eb17817
袁鹏 <yuanpeng@yuanpeng-MacBook.local>
2018-02-06 23:34:19 +0800

    rename

--- lib/directive/base-directive.go
+++ lib/directive/base-directive.go
@@ -1,9 +1,13 @@
 package directive
 
+import (
+	"github.com/satori/go.uuid"
+)
+
 type BaseDirective struct {
 	Type string `json:"type"`
 }
 
 func (this *BaseDirective) GenToken() string {
-	return "uuid"
+	return uuid.Must(uuid.NewV4()).String()
 }
